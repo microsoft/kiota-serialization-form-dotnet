f3816873528ed135b4dfdfca9b3515e15abf8380
Miha Markič <miha@rthand.com>
2024-05-04 11:57:38 +0200

    Implements IAsyncParseNodeFactory interface

--- src/FormParseNodeFactory.cs
+++ src/FormParseNodeFactory.cs
@@ -3,9 +3,9 @@ using Microsoft.Kiota.Abstractions.Serialization;
 namespace Microsoft.Kiota.Serialization.Form;
 
 /// <summary>
-/// The <see cref="IParseNodeFactory"/> implementation for form content types
+/// The <see cref="IAsyncParseNodeFactory"/> implementation for form content types
 /// </summary>
-public class FormParseNodeFactory : IParseNodeFactory
+public class FormParseNodeFactory : IAsyncParseNodeFactory
 {
     /// <inheritdoc/>
     public string ValidContentType => "application/x-www-form-urlencoded";
@@ -20,6 +20,21 @@ public class FormParseNodeFactory : IParseNodeFactory
 
         using var reader = new StreamReader(content);
         var rawValue = reader.ReadToEnd();
+        return new FormParseNode(rawValue);
+    }
+    /// <inheritdoc/>
+    public async Task<IParseNode> GetRootParseNodeAsync(string contentType, Stream content, 
+        CancellationToken cancellationToken = default)
+    {
+        if(string.IsNullOrEmpty(contentType))
+            throw new ArgumentNullException(nameof(contentType));
+        if(!ValidContentType.Equals(contentType, StringComparison.OrdinalIgnoreCase))
+            throw new ArgumentOutOfRangeException($"expected a {ValidContentType} content type");
+        if(content == null)
+            throw new ArgumentNullException(nameof(content));
+
+        using var reader = new StreamReader(content);
+        var rawValue = await reader.ReadToEndAsync();
         return new FormParseNode(rawValue);
     }
 }
